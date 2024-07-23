b86224b4bd9f6469f2766c89f3031c3d4297756f
Ivan Egorov <me@egori.ch>
2024-07-23 15:33:47 +0000

    started the fallible thingy

--- include/cordo/impl/core/cpo.hh
+++ include/cordo/impl/core/cpo.hh
@@ -1,11 +1,71 @@
 #pragma once
 
 #include <cstddef>
+#include <type_traits>
 
 #include "cordo/impl/core/macros.hh"
 #include "cordo/impl/core/meta.hh"
 
 namespace cordo_internal_cpo_core {
+struct fallible_tag final {};
+
+template <typename T, typename EH>
+concept fallible = requires {
+  requires std::is_same_v<typename T::tag_t, fallible_tag>;
+  typename T::ok_t;
+  typename T::err_t;
+};
+
+inline constexpr struct {
+  template <typename EH, fallible<EH> F>
+  constexpr auto err1_t(F &&) const noexcept {
+    return ::cordo::types_t<typename F::err_t>{};
+  }
+
+  template <typename T>
+  constexpr auto err1_t(T &&) const noexcept {
+    return ::cordo::types_t<>{};
+  }
+
+  template <typename EH, fallible<EH> F>
+  constexpr auto eh1_t(F &&) const noexcept {
+    return ::cordo::types_t<typename F::eh_t>{};
+  }
+
+  template <typename T>
+  constexpr auto eh1_t(T &&) const noexcept {
+    return ::cordo::types_t<>{};
+  }
+
+  constexpr auto common_t(cordo::types_t<>) const noexcept {
+    return cordo::null_t{};
+  }
+  template <typename... E>
+  constexpr auto common_t(cordo::types_t<E...>) const noexcept {
+    return ::cordo::tag_t<std::common_type_t<E...>>{};
+  }
+
+  template <typename... Args>
+  constexpr auto common_err_t(Args &&...a) const noexcept {
+    return this->common_t(::cordo::meta.concat(this->err1_t((Args &&)a)...));
+  }
+
+  template <typename... Args>
+  constexpr auto eh_t(Args &&...a) const noexcept {
+    // TODO: enforce all EH's are actually exactly the same.
+    return this->common_t(::cordo::meta.concat(this->eh1_t((Args &&)a)...));
+  }
+
+  template <typename EH, fallible<EH> F>
+  constexpr decltype(auto) result1(F &&v) const noexcept {
+    return EH::value((F &&)v);
+  }
+  template <typename T>
+  constexpr decltype(auto) result1(T &&v) const noexcept {
+    return (T &&)v;
+  }
+} fallible_helpers{};
+
 template <auto A>
 struct cpo_t final {};
 
@@ -44,6 +104,39 @@ struct invoke_t final {
   constexpr auto resolve(::cordo::overload_prio_t<1>, const algo<A> &a,
                          Args &&...args) const
       CORDO_INTERNAL_ALIAS_(A{}(a, (Args &&)args...));
+
+  template <typename A, typename... Args>
+  constexpr auto eh_wrapper(cordo::null_t, cordo::null_t, const algo<A> &a,
+                            Args &&...args) const
+      CORDO_INTERNAL_ALIAS_(this->resolve(::cordo::overload_prio_t<4>{}, a,
+                                          (Args &&)args...));
+
+  template <typename EH, typename E, typename A, typename... Args>
+  constexpr auto eh_wrapper(cordo::tag_t<EH>, cordo::tag_t<E>, const algo<A> &a,
+                            Args &&...args) const
+      noexcept(noexcept(EH::template make_result<E>(
+          this->eh_wrapper(cordo::null_t{}, cordo::null_t{}, a,
+                           fallible_helpers.result1((Args &&)args)...))))
+          -> decltype(EH::template make_result<E>(
+              this->eh_wrapper(cordo::null_t{}, cordo::null_t{}, a,
+                               fallible_helpers.result1((Args &&)args)...))) {
+    auto err = EH::template err_opt<E>();
+    bool proceed = ([&err]<typename T>(T &&v) {
+      if constexpr (fallible<decltype(v), EH>) {
+        if (!EH::success((T &&)v)) {
+          err = EH::template err_opt<E>(EH::err((T &&)v));
+          return false;
+        }
+        return true;
+      } else {
+        return true;
+      }
+    }(args) && ...);
+    return proceed ? EH::template make_result<E>(this->eh_wrapper(
+                         cordo::null_t{}, cordo::null_t{}, a,
+                         fallible_helpers.result1((Args &&)args)...))
+                   : err.move();
+  }
 
  public:
   template <auto A, typename... Args>
